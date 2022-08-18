2c61ca6f4e8039b0d05813e8ceff593c334a712f
oliverjam <oliverjamesphillips@gmail.com>
2022-08-18 16:10:06 +0100

    Emphasise reading test failure output

--- test/1.test.js
+++ test/1.test.js
@@ -6,5 +6,9 @@ test("/ returns hello world", async () => {
   const { status, body } = await request("/");
 
   assert.equal(status, 200);
-  assert.match(body, /<h1>hello express<\/h1>/i);
+  assert.match(
+    body,
+    /<h1>hello express<\/h1>/i,
+    `Expected HTML to include <h1>Hello Express</h1>, but received:\n${body}\n`
+  );
 });
