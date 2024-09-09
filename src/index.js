069040ac82a5ddca88c663b400a3e2dc9f9690f8
ShaughnAnderson94 <anderssji94@gmail.com>
2024-09-09 12:09:54 +0100

    #200 final tweaks

--- src/index.js
+++ src/index.js
@@ -2,3 +2,9 @@ const server = require("./server.js");
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => console.log(`Listening at http://localhost:${PORT}`));
+
+
+
+server.get("/", (req, res) => {
+  res.send(`<h1>Hello Express</h1>`);
+});
